faac7c1bcbc4b230564481b52179a810c0f39a92
Breno Martinusso <martinusso@gmail.com>
2014-05-05 09:44:04 -0300

    deleted commented code

--- Ini.Net/IniFile.cs
+++ Ini.Net/IniFile.cs
@@ -12,11 +12,6 @@ namespace Ini.Net
         private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
 
         public string FileName { get; private set; }
-        //private string fileName;
-        //public string GetFileName()
-        //{
-        //    return this.fileName;
-        //}
 
         public IniFile(string fileName)
         {
