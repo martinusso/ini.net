17700865984531891c25be3a62b8161c809c6115
Breno Martinusso <martinusso@gmail.com>
2014-05-05 10:57:33 -0300

    add doc to static extern methods

--- Ini.Net/IniFile.cs
+++ Ini.Net/IniFile.cs
@@ -6,10 +6,26 @@ namespace Ini.Net
 {
     public class IniFile
     {
+        /// <summary>
+        /// Copies a string into the specified section of an initialization file.
+        /// </summary>
+        /// <returns>
+        /// If the function successfully copies the string to the initialization file, the return value is nonzero.
+        /// If the function fails, or if it flushes the cached version of the most recently accessed initialization file, the return value is zero. 
+        /// </returns>
         [DllImport("kernel32")]
         private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+
+        /// <summary>
+        /// Retrieves a string from the specified section in an initialization file.
+        /// </summary>
+        /// <returns>
+        /// The return value is the number of characters copied to the buffer, not including the terminating null character.
+        /// </returns>
         [DllImport("kernel32")]
         private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
+        private static int size = 255;
 
         public string FileName { get; private set; }
 
@@ -25,16 +41,15 @@ namespace Ini.Net
 
         public string ReadString(string section, string key)
         {
-            StringBuilder temp = new StringBuilder(255);
-            int i = GetPrivateProfileString(section, key, null, temp, 255, this.FileName);
+            StringBuilder temp = new StringBuilder(size);
+            GetPrivateProfileString(section, key, null, temp, size, this.FileName);
             return temp.ToString();
         }
 
         public bool SectionExists(string sectionName)
         {
-            StringBuilder temp = new StringBuilder(255);
-            int i = GetPrivateProfileString(sectionName, null, null, temp, 255, this.FileName);
-            return temp.Length > 0;
+            int i = GetPrivateProfileString(sectionName, null, null, new StringBuilder(size), size, this.FileName);
+            return i > 0;
         }
     }
 }
